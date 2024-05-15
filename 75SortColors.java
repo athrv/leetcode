edaf2d395e211e215c3d1471143fd9e8168b5082
Atharva Tapkir <65921445+athrv@users.noreply.github.com>
2024-05-15 22:18:45 +0530

    Update 75SortColors.java

--- 75SortColors.java
+++ 75SortColors.java
@@ -1,3 +1,4 @@
+/* Two Pointer Solution */
 class Solution {
     public void sortColors(int[] nums) {
         sorter(nums, 0, 0);
@@ -21,5 +22,30 @@ class Solution {
             }
         }
         if(num<3 && i<nums.length) sorter(nums, ++num, i);
+    }
+}
+
+/* Dutch Flag Algorithm */
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high) {
+            if(nums[mid]==0) {
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+            } else if(nums[mid]==1) {
+                mid++;
+            } else {
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
     }
 }
