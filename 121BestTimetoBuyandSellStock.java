9b075b4f245097a865e72f05bed7548788e4b155
Atharva Tapkir <65921445+athrv@users.noreply.github.com>
2024-05-18 13:36:42 +0530

    Update 121BestTimetoBuyandSellStock.java

--- 121BestTimetoBuyandSellStock.java
+++ 121BestTimetoBuyandSellStock.java
@@ -14,3 +14,17 @@ class Solution {
         return profit;
     }
 }
+
+/* Dynamic Programming */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        int minBuy=prices[0];
+        for(int i=1; i<prices.length; i++) {
+            int cost=prices[i]-minBuy;
+            profit=Math.max(profit, cost);
+            minBuy=Math.min(minBuy, prices[i]);
+        }
+        return profit;
+    }
+}
