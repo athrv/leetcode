414892a6ee59f56512320589510c5b97553bb459
Atharva Tapkir <65921445+athrv@users.noreply.github.com>
2024-05-18 12:29:14 +0530

    Update 53MaximumSubarray.java

--- 53MaximumSubarray.java
+++ 53MaximumSubarray.java
@@ -15,3 +15,26 @@ class Solution {
         return (int) maxSum;
     }
 }
+
+/* Printing the Subarray */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        long maxSum=Long.MIN_VALUE;
+        int start=0; 
+        int ansStart=-1, ansEnd=-1;
+        for(int i=0; i<nums.length; i++) {
+            if(sum==0) start=i;
+            sum+=nums[i];
+            if(sum>maxSum) {
+                maxSum=sum;
+                ansStart=start;
+                ansEnd=i;
+            }
+            if(sum<0) {
+                sum=0;
+            }
+        }
+        return (int) maxSum;
+    }
+}
