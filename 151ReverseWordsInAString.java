da4bc8c7615ee86d4b70c9dd01620f9d676c4c3b
Atharva Tapkir <65921445+athrv@users.noreply.github.com>
2024-06-01 11:24:21 +0530

    Used StringBuilder for optimising

--- 151ReverseWordsInAString.java
+++ 151ReverseWordsInAString.java
@@ -1,15 +1,15 @@
 class Solution {
     public String reverseWords(String s) {
         int i=s.length()-1;
-        String res="";
+        StringBuilder res=new StringBuilder();
         while(i>=0) {
             while(i>=0 && s.charAt(i)==' ') i--;
             int j=i;
             if(i<0) break;
             while(i>=0 && s.charAt(i)!=' ') i--;
-            if(res.isEmpty()) res=res.concat(s.substring(i+1, j+1));
-            else res=res.concat(" "+s.substring(i+1, j+1));
+            if(res.length()>0) res.append(" ");
+            res.append(s.substring(i+1, j+1));
         }
-        return res;
+        return res.toString();
     }
 }
