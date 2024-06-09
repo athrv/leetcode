3f10e149c61df2cb0b34dbd3a1b5b206ea3602ab
Atharva Tapkir <65921445+athrv@users.noreply.github.com>
2024-06-09 14:22:10 +0530

    Create IntroductionToLinkedList.java

---
+++ miscellaneous/IntroductionToLinkedList.java
@@ -0,0 +1,22 @@
+/*
+class Node { 
+    int data; 
+    Node next; 
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+} 
+*/
+class Solution {
+    static Node constructLL(int arr[]) {
+        // code here
+        Node head=new Node(arr[0]);
+        Node mover=head;
+        for(int i=1; i<arr.length; i++) {
+            Node temp=new Node(arr[i]);
+            mover.next=temp;
+            mover=temp;
+        }
+        return head;
+    }
+}
