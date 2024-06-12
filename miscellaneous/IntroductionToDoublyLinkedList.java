6133fc52ee4989fe6780773c1088f1643b2396ad
Atharva Tapkir <65921445+athrv@users.noreply.github.com>
2024-06-12 22:47:42 +0530

    Create IntroductionToDoublyLinkedList.java

---
+++ miscellaneous/IntroductionToDoublyLinkedList.java
@@ -0,0 +1,14 @@
+class Solution {
+    Node constructDLL(int arr[]) {
+        // Code here
+        Node head=new Node(arr[0]);
+        Node prev=head;
+        for(int i=1; i<arr.length; i++) {
+            Node temp=new Node(arr[i]);
+            prev.next=temp;
+            temp.prev=prev;
+            prev=temp;
+        }
+        return head;
+    }
+}
